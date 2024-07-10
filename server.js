7ec94cde2e67d90436115fc5d0ad8b7632dcf408
alex3952 <alex1234440@gmail.com>
2024-07-10 10:24:17 -0500

    update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_API)
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -19,3 +21,7 @@ app.use('/api', require('./routes/api'));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use('/api', require('./routes/api'));
